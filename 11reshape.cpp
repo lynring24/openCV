f3f4f23812441195a11d3bf4da61bcb224071c38
Herin Chung <lync2846@gmail.com>
2018-11-15 14:35:31 +0900

    change the ratio due to type error

--- 11reshape.cpp
+++ 11reshape.cpp
@@ -5,18 +5,18 @@ using namespace cv;
 void main() {
 	Mat src = imread("Mandrill_g.bmp", IMREAD_GRAYSCALE);
 	Mat dst = Mat::zeros(src.size()*3, src.type());
-	int const RATIO = 0.3;
+	double const RATIO = 1.0/3.0;
 
 	for(int i=0; i< dst.rows; i++)
 		for( int j=0; j< dst.cols; j++) {
 			int r = i / 3;
 			int c = j / 3;
-			int m1 = src.at<uchar>(r, c)  + RATIO * (src.at<uchar>(r, c+1) - src.at<uchar>(r, c));
-			int m2 = src.at<uchar>(r +1, c) + RATIO * (src.at<uchar>(r +1, c+1) - src.at<uchar>(r +1, c));
-			int p = m1 + RATIO * (m2 - m1);
+			double m1 = src.at<uchar>(r, c)  + RATIO * (src.at<uchar>(r, c+1) - src.at<uchar>(r, c));
+			double m2 = src.at<uchar>(r +1, c) + RATIO * (src.at<uchar>(r +1, c+1) - src.at<uchar>(r +1, c));
+			double p = m1 + RATIO * (m2 - m1);
 			dst.at<uchar>(i, j) = p;
 		}
-
+	imshow("original", src);
 	imshow("extension", dst);
 	waitKey(0);
 }
